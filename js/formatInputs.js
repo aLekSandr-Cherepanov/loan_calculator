0288aa95189ab8e483d2f4a82b04024cd84d312c
Alexander Cherepanov <super.sany96@yandex.ru>
2023-01-26 16:23:23 +0300

    продолжение написание кода

--- js/formatInputs.js
+++ js/formatInputs.js
@@ -4,8 +4,27 @@ const inputConst = document.querySelector('#input-cost');
 const inputDownPayment = document.querySelector('#input-downpayment');
 const inputTerm = document.querySelector('#input-term');
 
+//Cleave опции форматирования
 
-var cleave = new Cleave('#input-cost', {
+const cleavePriceSetting = {
     numeral: true,
-    numeralThousandsGroupStyle: 'thousand'
-});
+    numeralThousandsGroupStyle: 'thousand',
+    delimiter: ' '
+};
+
+//Запускаем форматирование Cleave
+const cleaveCost = new Cleave(inputConst, cleavePriceSetting);
+const cleaveDownPayment = new Cleave(inputDownPayment,cleavePriceSetting);
+
+//Отображение рассчета суммы кредита 
+form.addEventListener('input', function () {
+
+    //Сумма кредита
+
+    const totalAmount = +cleaveCost.getRawValue() - cleaveDownPayment.getRawValue();
+    console.log(totalAmount);
+
+    totalCost.innerText = totalAmount;
+
+    
+})
